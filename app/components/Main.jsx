419a12d72fef3b63a6b59e31f2a811a3b9c7efa5
Amaan Shaikh <shaikhamaan257@gmail.com>
2017-10-18 13:49:01 +0530

    Starting the count

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,9 @@ var Navigation =  require('Navigation');
 var Main = (props) => {
   return (
     <div>
-     <Navigation/>
+    <Navigation/>
      <div className="row">
-        <div className="column small-centered medium-6 large-4">
+        <div className="column medium-6 large-4">
             {props.children}
         </div>
      </div>
