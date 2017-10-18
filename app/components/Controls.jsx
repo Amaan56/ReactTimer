4752478f9f00a0f05828e5e646f25f590dbf7ae9
Amaan Shaikh <shaikhamaan257@gmail.com>
2017-10-18 18:55:52 +0530

    Checking Controls with Reference

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,35 +1,32 @@
 var React = require('react');
-var CountdownForm = require('CountdownForm');
 
 var Controls = React.createClass({
-  propTypes : {
-    countdownStatus : React.PropTypes.string.isRequired,
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired,
     onStatusChange: React.PropTypes.func.isRequired
   },
-  onStatusChange:function (newStatus) {
-    return () =>{
+  onStatusChange: function (newStatus) {
+    return () => {
       this.props.onStatusChange(newStatus);
     }
   },
-  render:function () {
-    var {countdownStatus} =  this.props;
-    var renderStartStopButton = function () {
-      var that = this;
-      if(countdownStatus==="started"){
-        return <button className="button secondary" onClick={that.onStatusChange('paused')}>Pause</button>
-      }else if(countdownStatus==="paused"){
-          return <button className="button primary" onClick={that.onStatusChange('started')}>Start</button>
+  render: function () {
+    var {countdownStatus} = this.props;
+    var renderStartStopButton = () => {
+      if (countdownStatus === 'started') {
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
+      } else if (countdownStatus === 'paused') {
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
-    return(
+
+    return (
       <div className="controls">
         {renderStartStopButton()}
         <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
-
-    );
+    )
   }
 });
 
-
-module.exports =  Controls;
+module.exports = Controls;
